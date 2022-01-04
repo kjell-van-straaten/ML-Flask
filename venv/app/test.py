65b90c8edaf655fc68ff84bd3af61d01d2dcb8a0
kjellvs <64495619+kjellvs@users.noreply.github.com>
2022-01-04 14:46:49 +0100

    update model

--- venv/app/test.py
+++ venv/app/test.py
@@ -1,9 +1,21 @@
-from app.functions import *
+# from app.functions import *
 
-db = connect_db()
-account_table = db.Accounts
-machinedata_table = db.MachineData
+# db = connect_db()
+# account_table = db.Accounts
+# machinedata_table = db.MachineData
 
-historic_data = list(machinedata_table.find())
+# historic_data = list(machinedata_table.find())
 
-print(historic_data)
+# print(historic_data)
+
+# import joblib
+# from sklearn import tree
+
+# X = [[45],[55]]
+# Y = [0, 1]
+# clf = tree.DecisionTreeClassifier()
+# clf = clf.fit(X, Y)
+
+# print(clf.predict([[55]])[0])
+
+# joblib.dump(clf, 'classifier.pkl')
